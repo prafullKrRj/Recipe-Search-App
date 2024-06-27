dfda181fee7f06935600a6f3eec139a7c4692257
prafullKrRj <prafullkumar384@gmail.com>
2024-06-27 17:45:46 +0530

    Version 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -106,6 +106,4 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     kapt(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
-
-    implementation(libs.generativeai)
 }
